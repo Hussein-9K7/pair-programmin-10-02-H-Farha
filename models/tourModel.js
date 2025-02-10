943a3ccdeed79515dc939ebe4dfc17faae037342
Hussein <alprstigehussein@gmail.com>
2025-02-10 23:42:45 +0200

    i add some thing i learnd

--- models/tourModel.js
+++ models/tourModel.js
@@ -6,30 +6,17 @@
   "price": "2,195"
 }
 */ 
-const mongoose = require("mongoose");
-
-const Schema = mongoose.Schema;
-
-const tourSchema = new Schema(
+const mongoose = require ('mongoose');
+const Schema = mongoose.Schema
+const tourSchema = new Schema (
   {
-    name: {
-      type: String,
-      required: true,  
-    },
-    info: {
-      type: String,
-      required: true,  
-    },
-    image: {
-      type: String,
-      required: true, 
-    },
-    price: {
-      type: String,
-      required: true,  
-    },
+    name : {type: String , required : true },
+    email : {type : String , required : true },
+    image : {type : String , required : true },
+    price : {type : String , required : true},
   },
-  { timestamps: true } 
+
+  {timestamps : true}
 );
 
-module.exports = mongoose.model("Tour", tourSchema);  
+module.exports = mongoose.model ("Toour" , tourSchema);
--- models/userModel.js
+++ models/userModel.js
@@ -7,44 +7,17 @@
   "date_of_birth": "2000-01-15",
   "membership_status": "Active"
 } */
-  const mongoose = require("mongoose");
-
-  const Schema = mongoose.Schema;
-  
-  const userSchema = new Schema(
-    {
-      name: {
-        type: String,
-        required: true,
-      },
-      email: {
-        type: String,
-        required: true,
-        unique: true, 
-      },
-      password: {
-        type: String,
-        required: true,
-      },
-      phone_number: {
-        type: String,
-        required: true,
-      },
-      gender: {
-        type: String,
-        required: true,
-      },
-      date_of_birth: {
-        type: Date,
-        required: true,
-      },
-      membership_status: {
-        type: String,
-        required: true,
-      },
-    },
-    { timestamps: true } 
-  );
-  
-
-  module.exports = mongoose.model("User", userSchema);
+const mongoose = require ('mongoose');
+const Schema = mongoose.Schema 
+const userSchema = new Schema (
+  {
+    name : {type : String , required : true },
+    email : {type : String , required : true , unique : true },
+    password : {type : String , required : true },
+    phone_number : {type : String , required : true } ,
+    gender : {type : String , required : true } ,
+    date_of_birth : {type : String , required : true }, 
+    membership_status : {type : String , required : true}
+  },
+  {timestamps : true}
+);
